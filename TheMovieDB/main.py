6de30fffe0eb3260bbf55230a166696e3fff9610
horvathattila <horvath.attila1105@icloud.com>
2021-02-28 16:36:25 +0100

    new

--- TheMovieDB/main.py
+++ TheMovieDB/main.py
@@ -6,6 +6,7 @@ from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 
 
+
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
